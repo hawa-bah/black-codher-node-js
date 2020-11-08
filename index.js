87ba1cb0182478f08b261637963d1a378f1c62f7
hawa-bah <56238080+hawa-bah@users.noreply.github.com>
2020-11-08 19:37:53 +0000

    Tiding the code

--- index.js
+++ index.js
@@ -25,8 +25,8 @@ const db = {
       author: 'Suzanne Collins'
     },
     1: {
-        title: 'The fault in our stars',
-        author: 'John Green'
+      title: 'The fault in our stars',
+      author: 'John Green'
     }
   }
 }
@@ -81,35 +81,35 @@ app.post('/profiles', (req, res) => {
 // DELETE profiles
 app.delete('/profiles/1000', (req, res) => {
 
-    delete db.profiles[1000] 
+  delete db.profiles[1000] 
 
-    res.json({
-        message: 'The profile 1000 has been deleted'
-    })
+  res.json({
+      message: 'The profile 1000 has been deleted'
+  })
 
 })
 app.delete('/profiles/:userId', (req, res) => {
 
-    const idToDelete =[req.params.userId]
+  const idToDelete =[req.params.userId]
 
-    delete db.profiles[idToDelete]
+  delete db.profiles[idToDelete]
 
-    res.json({
-        message: 'The profile has been deleted'
-    })
+  res.json({
+      message: 'The profile has been deleted'
+  })
 
 })
 
 //PUT  
 app.put('/profiles/:userId', (req, res) => {
 
-    const idToPut = req.params.userId
-    // updating 
-    db.profiles[idToPut] = req.body
+  const idToPut = req.params.userId
+  // updating 
+  db.profiles[idToPut] = req.body
 
-    res.json({
-        message: 'The profile has been overwritten (put)'
-    })
+  res.json({
+      message: 'The profile has been overwritten (put)'
+  })
 
 })
 app.patch('/profiles/:userId', (req, res) => {
@@ -119,7 +119,7 @@ app.patch('/profiles/:userId', (req, res) => {
   db.profiles[idToPatch] = {...db.profiles[idToPatch],...req.body }
 
   res.json({
-      message: 'The profile has been modified (path)'
+    message: 'The profile has been modified (path)'
   })
 
 })
@@ -138,25 +138,24 @@ app.get('/', (req, res) => {
 //C POST Books
 app.post('/books', (req,res)=>{
 
-    const existingBooks = Object.keys(db.books)
-    const largestKey = Math.max(...existingBooks)
+  const existingBooks = Object.keys(db.books)
+  const largestKey = Math.max(...existingBooks)
+  const newKey = largestKey + 1
 
-    const newKey = largestKey + 1
+  db.books[newKey] = req.body
 
-    db.books[newKey] = req.body
-
-    res.json({
-        status: 'success',
-        message: `Created a book with id of ${newKey}`
-    })
+  res.json({
+      status: 'success',
+      message: `Created a book with id of ${newKey}`
+  })
 })
 
 // R GET books
 app.get('/books', (req,res)=>{
-    res.json({
-        title: 'All the books',
-        data: db.books
-    })
+  res.json({
+      title: 'All the books',
+      data: db.books
+  })
 })
 app.get('/books/:bookId', (req,res)=>{
 
@@ -193,7 +192,7 @@ app.patch('/books/:bookId', (req, res) => {
   db.books[idToPatch] = {...db.books[idToPatch],...req.body}
 
   res.json({
-      message: 'The book has been modified '
+    message: 'The book has been modified '
   })
 })
 // D
@@ -201,10 +200,12 @@ app.delete('/books:bookId', (req, res) => {
   delete db.books[req.params.bookId]
 
   res.json({
-    message: 'the book with has been deleted' 
+    message: 'the book has been deleted' 
   })
 })
 
 app.listen(4000, () => {
   console.log('server is running!')
 })
+
+// run node index.js to start server
